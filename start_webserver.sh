5fb59ea3abc49b399cc10deaa2d7a339e8330784
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-07-31 14:36:36 -0400

    Update start_webserver.sh

--- start_webserver.sh
+++ start_webserver.sh
@@ -1,14 +1,2 @@
 #!/usr/bin/env bash
-
-# https://elrey.casa/bash/scripting/harden
-set -${-//[sc]/}eu${DEBUG+xv}o pipefail
-
-function main(){
-  # starting a webserver
-  python3 -m http.server
-}
-
-# https://elrey.casa/bash/scripting/main
-if [[ "${0}" = "${BASH_SOURCE[0]:-bash}" ]] ; then
-  main "${@}"
-fi
+python3 -m http.server
